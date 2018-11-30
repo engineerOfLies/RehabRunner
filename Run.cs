11a78b366c05b4d2f7d1700cffeaf86652fe40c1
Reginald <rlw8@njit.edu>
2018-11-30 15:28:06 -0500

    Initial commit w/ 'run' script

---
+++ Run.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Run : MonoBehaviour {
+
+    
+    public GameObject player;
+    public Rigidbody pBody;
+    float speedMod;
+    Vector3 zVec;
+
+    [Range(1,3)]
+    public int speed;
+    public float thrust;
+
+    bool grounded;
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+
+        zVec = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        speedMod = (Time.deltaTime / 100000) + 1;
+
+        if (speedMod != 0) 
+        {
+            zVec.z = (speed + speedMod);
+            player.transform.position += zVec;
+        }
+
+        
+        //Its a jetpack I guess
+        
+        if(Input.GetAxis("Jump")!= 0 && grounded)
+        {
+            pBody.AddForce(transform.up * thrust);
+            Debug.Log("Yump");
+
+            grounded = false;
+        }
+        
+	}
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plane")
+            grounded = true;
+    }
+}
